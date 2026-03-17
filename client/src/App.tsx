e4861d37589288f7931af78e1a2e506b88461435
meshulamzusha <nauko763@gmail.com>
2026-03-17 18:13:50 +0200

    call the protected route component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LauncherDetailsPage from "./pages/LauncherDetailsPage";
+import Login from "./pages/Login";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/launchers/:id" element={<LauncherDetailsPage />} />
+        <Route path="/" element={<Login />} />
+        <Route
+          element={
+            <ProtectedRoute
+              allowedUserTypes={["admin", "air", "intelligence"]}
+            />
+          }
+        >
+          <Route path="/launchers" element={<HomePage />} />
+          <Route path="/launchers/:id" element={<LauncherDetailsPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
