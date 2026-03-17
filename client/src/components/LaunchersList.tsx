8d999fe554353d6e7b79319222578e25a655377e
meshulamzusha <nauko763@gmail.com>
2026-03-17 09:50:18 +0200

    add navigation to launcher page onClick

--- client/src/components/LaunchersList.tsx
+++ client/src/components/LaunchersList.tsx
@@ -1,13 +1,14 @@
 import { useEffect } from "react";
 import { useLaunchers } from "../stores/launchers.store";
 import type { LaunchersState, Launcher } from "../types/launchers.types";
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const LaunchersList = () => {
   const launchers = useLaunchers((state: LaunchersState) => state.launchers);
   const fetchLaunchers = useLaunchers(
     (state: LaunchersState) => state.fetchLaunchers,
   );
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetchLaunchers();
@@ -15,10 +16,19 @@ const LaunchersList = () => {
 
   return (
     <div>
-        Launchers List
+      Launchers List
       <ul>
         {launchers.map((l: Launcher) => (
-          <li key={l._id} onClick={()=>{}}>{Object.values(l).map(v => <div key={v}>{v}</div>)}</li>
+          <li
+            key={l._id}
+            onClick={() => {
+              navigate(`/launchers/${l._id}`);
+            }}
+          >
+            {Object.values(l).map((v) => (
+              <div key={v}>{v}</div>
+            ))}
+          </li>
         ))}
       </ul>
     </div>
